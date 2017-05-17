2aa46a3da33fc3d206e668ff9407af9c7f931c02
SummittDweller <Summitt.Dweller@gmail.com>
2017-05-17 07:39:44 -0400

    Added migrate variables.

--- rootstalk.vars.py
+++ rootstalk.vars.py
@@ -1,17 +1,19 @@
 """ backup.d/vars.py
 
-This Python script defines variables for use by backup.py and restore.py.
+This Python script defines variables for use by migrate.py.
 Define some critical vars.  These need to be modified for each project!  Use 'drush status' to determine some values.
 
-Customized 17-Jan-2017 for Rootstalk.Grinnell.edu.
+Customized 26-Apr-2017 to restore a backup from @rootstalk.prod to @rootstalk.dev.
 
 """
 
 backup = "rootstalk.tar.gz"                                       # name the backup file
 stick = "/Volumes/RS"                                             # name of a mounted drive to accept a copy of the backup
-server = "rootstalk.grinnell.edu"                                 # the remote server name
-user = "dguser"                                                   # admin user on the remote server
-drush_alias = "@rootstalk.prod"                                   # drush alias for the remote site
-web_path = "/var/www/html/drupal/web/"                            # path to the Drupal web root on the remote server
+server = "rootstalk.grinnell.dev"                                 # the remote server name
+backup_server = "rootstalk.grinnell.edu"                          # the remote server that the backup came from
+user = "vagrant"                                                  # admin user on the remote server
+drush_alias = "@rootstalk.dev"                                    # drush alias for the remote site
+web_path = "/var/www/drupal/web/"                                 # path to the Drupal web root on the remote server
 site_path = web_path + "sites/default"                            # path to the Drupal site on the remote server
-drush = "/var/www/html/drupal/vendor/drush/drush/drush.php"       # remote server path to drush
+drush = "/var/www/drupal/vendor/drush/drush/drush.php"            # remote server path to drush
+
--- vars.py
+++ vars.py
@@ -1,19 +1,17 @@
 """ backup.d/vars.py
 
-This Python script defines variables for use by migrate.py.
+This Python script defines variables for use by backup.py, restore.py and migrate.py.
 Define some critical vars.  These need to be modified for each project!  Use 'drush status' to determine some values.
-
-Customized 26-Apr-2017 to restore a backup from @rootstalk.prod to @rootstalk.dev.
 
 """
 
-backup = "rootstalk.tar.gz"                                       # name the backup file
-stick = "/Volumes/RS"                                             # name of a mounted drive to accept a copy of the backup
-server = "rootstalk.grinnell.dev"                                 # the remote server name
-backup_server = "rootstalk.grinnell.edu"                          # the remote server that the backup came from
-user = "vagrant"                                                  # admin user on the remote server
-drush_alias = "@rootstalk.dev"                                    # drush alias for the remote site
-web_path = "/var/www/drupal/web/"                                 # path to the Drupal web root on the remote server
-site_path = web_path + "sites/default"                            # path to the Drupal site on the remote server
-drush = "/var/www/drupal/vendor/drush/drush/drush.php"            # remote server path to drush
-
+backup = "wieting.tar.gz"                                         # name the backup file
+stick = "/Volumes/WIETING"                                        # name of a mounted drive to accept a copy of the backup
+server = "wieting.tamatoledo.com"                                 # the remote server name
+backup_server = "wieting.dev"                                     # used by migrate.py to identify the destination server
+user = "admin"                                                    # admin user on the remote server
+drush_alias = "@wieting"                                          # drush alias for the remote site
+web_path = "/var/www/drupalvm/drupal/web/"                        # path to the Drupal web on the remote server
+site_path = web_path + "sites/wieting"                            # path to the Drupal site on the remote server
+backup_path = web_path + "sites/default"                          # used by migrate.py to identify the destination site directory
+drush = "/var/www/drupalvm/drupal/vendor/drush/drush/drush.php"   # remote server path to drush
